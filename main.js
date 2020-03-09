e1c1e661094143f738d97e8c0d6ed7597caff953
ronyonka <rononyonka@gmail.com>
2020-03-09 11:12:49 +0300

    added v-bind

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var app= new Vue({
     'el':'#app',
     'data':{
-        'product':'socks',
-        'description':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque et qui magni minus doloribus. Laboriosam, necessitatibus repellendus laborum facilis minima quas? Cupiditate dolore asperiores repellendus quis eaque enim, est adipisci?'
+        'product':'Socks',
+        'image':'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        'link':'https://ronyonka.github.io'
     }
 })
