ecaaf8c872f4a0b39b927e5f197fe30253181d2e
ronyonka <rononyonka@gmail.com>
2020-03-09 10:57:49 +0300

    linked vue to html

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-var product = 'socks'
+var app= new Vue({
+    'el':'#app',
+    'data':{
+        'product':'socks',
+        'description':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque et qui magni minus doloribus. Laboriosam, necessitatibus repellendus laborum facilis minima quas? Cupiditate dolore asperiores repellendus quis eaque enim, est adipisci?'
+    }
+})
